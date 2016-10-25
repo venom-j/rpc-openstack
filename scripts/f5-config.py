6c3252a99896728c08e468c1ed57874c85742bcf
David Pham <david.pham@rackspace.com>
2016-10-25 11:39:48 -0500

    Added SWIFT port :8080 to AFM rules.
    Added SWIFT port :8080 to AFM rules.

--- scripts/f5-config.py
+++ scripts/f5-config.py
@@ -155,7 +155,7 @@ SEC_AFM_RULES = (
     '\n### CREATE AFM LIST AND RULES ###\n'
     #Port Lists
     'create security firewall port-list RPC_VIP_PORTS '
-    '{ ports add { 80 { } 443 { } 3306 { } 5000 { } 6082 { } 8000 { } 8003 { } 8004 { } 8181 { } 8443 '
+    '{ ports add { 80 { } 443 { } 3306 { } 5000 { } 6082 { } 8000 { } 8003 { } 8004 { } 8080 { } 8181 { } 8443 '
     '{ } 8775 { } 8776 { } 8888 { } 9191 { } 9200 { } 9292 { } 9696 { } 35357 { } } }\n'
     '\n'
     #Addr Lists
