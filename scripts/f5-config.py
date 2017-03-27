9c6207a45dfa7f1957b9aff9ee85c17d923824db
David Pham <david.pham@rackspace.com>
2017-03-27 12:14:08 -0500

    Partial Merge with Upstream
    Fixed issue with Galera loads when an F5 failsover
    Added REPO_GIT and MAGNUM pools
    Removed connection-limit from poor man's AFM forwarders

--- scripts/f5-config.py
+++ scripts/f5-config.py
@@ -75,7 +75,12 @@ MONITORS = [
     r' tcp destination *:9200 }',
     r'create ltm monitor http /' + PART + '/' + PREFIX_NAME + '_MON_HTTP_REPO {'
     r' defaults-from http destination *:8181 recv "200 OK" send "HEAD /'
-    r' HTTP/1.1\r\nHost: rpc\r\n\r\n" }'
+    r' HTTP/1.1\r\nHost: rpc\r\n\r\n" }',
+    r'create ltm monitor http ' + PART + PREFIX_NAME + '_MON_HTTP_REPO_CACHE {'
+    r' defaults-from http destination *:3142 recv "200 OK" send "HEAD /acng-report.html'
+    r' HTTP/1.1\r\nHost: rpc\r\n\r\n" }',
+    r'create ltm monitor tcp ' + PART + PREFIX_NAME + '_MON_TCP_REPO_GIT {'
+    r' defaults-from tcp destination *:9418 }',
     '\n'
 ]
 
@@ -94,6 +99,7 @@ POOL_NODE = {
                  ' load-balancing-mode least-connections-node members replace-all-with'
                  ' { %(nodes)s }',
     'priority': 'min-active-members 1',
+    'service-reset': 'service-down-action reset slow-ramp-time 0',
     'end': 'monitor %(mon_type)s }'
 }
 
@@ -117,6 +123,7 @@ VIRTUAL_ENTRIES = (
     r' profiles replace-all-with { /Common/fastL4 { } }'
     '  %(persist)s'
     ' source 0.0.0.0/0'
+    ' %(mirror_status)s'
     ' source-address-translation { pool /' + PART + '/' + PREFIX_NAME + '_SNATPOOL type snat }'
     ' }'
 )
@@ -155,8 +162,8 @@ SEC_AFM_RULES = (
     '\n### CREATE AFM LIST AND RULES ###\n'
     #Port Lists
     'create security firewall port-list RPC_VIP_PORTS '
-    '{ ports add { 80 { } 443 { } 3306 { } 3307 { } 5000 { } 6082 { } 8000 { } 8003 { } 8004 { } 8080 { } '
-    '8181 { } 8443 { } 8774 { } 8775 { } 8776 { } 8888 { } 9191 { } 9200 { } 9292 { } 9696 { } 35357 { } } }\n'
+    '{ ports add { 80 { } 443 { } 3306 { } 5000 { } 6082 { } 8000 { } 8003 { } 8004 { } 8080 { } 8181 { } 8443 '
+    '{ } 8774 { } 8775 { } 8776 { } 8888 { } 9191 { } 9200 { } 9292 { } 9696 { } 35357 { } } }\n'
     '\n'
     #Addr Lists
     'create security firewall address-list RPC_PUB_VIP_ALLOW_IPS { addresses add { 0.0.0.0/0 } }\n'
@@ -189,7 +196,7 @@ SEC_AFM_RULES = (
 
 SEC_CONTAINER_VIRTUAL_ENTRIES = (
     'create ltm virtual /' + PART + '/' + PREFIX_NAME + '_LIMIT_ACCESS_TO_CONTAINER_NET {'
-    ' connection-limit 1 destination %(sec_container_net)s:0 ip-forward mask'
+    ' destination %(sec_container_net)s:0 ip-forward mask'
     ' %(sec_container_netmask)s profiles replace-all-with'
     ' { /Common/fastL4 { } } rules { /' + PART + '/' + PREFIX_NAME + '_DISCARD_ALL'
     ' } translate-address disabled translate-port disabled'
@@ -204,6 +211,17 @@ POOL_PARTS = {
         'mon_type': '/' + PART + '/' + PREFIX_NAME + '_MON_GALERA',
         'priority': True,
         'group': 'galera',
+        'service-reset': True,
+        'connection-mirror': True,
+        'hosts': []
+    },
+    'galera_read': {
+        'port': 3307,
+        'backend_port': 3306,
+        'mon_type': '/' + PART + '/' + PREFIX_NAME + '_MON_GALERA',
+        'priority': False,
+        'group': 'galera',
+        'connection-mirror': True,
         'hosts': []
     },
     'glance_api': {
@@ -367,6 +385,28 @@ POOL_PARTS = {
         'mon_type': '/' + PART + '/' + PREFIX_NAME + '_MON_HTTP_REPO',
         'group': 'pkg_repo',
         'priority': True,
+        'hosts': []
+    },
+    'repo_cache': {
+        'port': 3142,
+        'backend_port': 3142,
+        'mon_type': PART + PREFIX_NAME + '_MON_HTTP_REPO_CACHE',
+        'group': 'repo_all',
+        'priority': True,
+        'hosts': []
+    },
+    'repo_git': {
+        'port': 9418,
+        'backend_port': 9418,
+        'mon_type': PART + PREFIX_NAME + '_MON_TCP_REPO_GIT',
+        'group': 'pkg_repo',
+        'hosts': []
+    },
+    'magnum': {
+        'port': 9511,
+        'backend_port': 9511,
+        'mon_type': PART + 'RPC-MON-EXT-ENDPOINT',
+        'group': 'magnum_all',
         'hosts': []
     }
 }
@@ -567,9 +607,7 @@ def args():
         action='store_true'
     )
 
-
     return vars(parser.parse_args())
-
 
 def main():
     """Run the main application."""
@@ -609,12 +647,10 @@ def main():
     if user_args['ssl_domain_name']:
         commands.extend([
             '### UPLOAD SSL CERT KEY PAIR  ###',
-            'cd /RPC',
             'create sys crypto key /' + PART + '/%(ssl_domain_name)s.key'
             % user_args,
             'create sys crypto cert /' + PART + '/%(ssl_domain_name)s.crt key /' % user_args + PART + '/%(ssl_domain_name)s.key common-name %(ssl_domain_name)s lifetime 3650'
             % user_args,
-            'cd /Common\n',
             '### CREATE SSL PROFILES ###',
             ('create ltm profile client-ssl'
             ' /' + PART + '/' + PREFIX_NAME + '_PROF_SSL_%(ssl_domain_name)s'
@@ -666,9 +702,14 @@ def main():
                 persist = PERSIST_OPTION
             else:
                 persist = str()
+            if value.get('connection-mirror'):
+                mirror_state = 'mirror enabled'
+            else:
+                mirror_state = str()
 
             virtual_dict = {
                 'port': value['port'],
+                'mirror_status': mirror_state,
                 'vs_name': value['vs_name'],
                 'pool_name': value['pool_name'],
                 'internal_lb_vip_address': lb_vip_address,
@@ -676,6 +717,7 @@ def main():
                 'ssl_domain_name': user_args['ssl_domain_name'],
                 'ssl_public_ip': user_args['ssl_public_ip'],
             }
+
 ##########################################
             virt = '%s' % VIRTUAL_ENTRIES % virtual_dict
             if virt not in virts:
@@ -728,7 +770,8 @@ def main():
         pool_node = [POOL_NODE['beginning'] % value]
         if value.get('priority') is True:
             pool_node.append(POOL_NODE['priority'])
-
+        if value.get('service-reset') is True:
+            pool_node.append(POOL_NODE['service-reset'])
         pool_node.append(POOL_NODE['end'] % value)
         pools.append('%s' % ' '.join(pool_node))
 
@@ -747,9 +790,9 @@ def main():
     for snat_ip in snat_pool_adds.split(","):
         snat_translations.append( SNAT_IDLE % snat_ip)
 
+
     script = [
         '#!/usr/bin/bash\n',
-        r'### F5 Build Script -- Liberty ###',
         r'### CREATE RPC PARTITION ###',
         'create auth partition %s\n' % PART,
         r'### SET DISPLAY PORT NUMBERS ###',
@@ -764,7 +807,7 @@ def main():
     script.extend(['### CREATE PERSISTENCE PROFILES ###'])
     script.extend(['%s' % i % user_args for i in PERSISTANCE])
     script.extend(['### CREATE NODES ###'])
-    script.extend(['%s' % i % user_args for i in sorted(nodes)])
+    script.extend(['%s' % i % user_args for i in nodes])
     script.extend(['\n### CREATE POOLS ###'])
     script.extend(pools)
     script.extend(['\n### CREATE VIRTUAL SERVERS ###'])
